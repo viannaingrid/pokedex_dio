eb57208598f9fdd8a41e8acdc358313a0cef37ed
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-11-28 16:52:08 -0300

    :bricks: update js with fetch and json elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,32 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="assets/img/bulbasaur.png" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToLi(pokemon))
+
+            document.getElementsByClassName('pokemons')
+        }
+        console.log(pokemonList)
+    })
     .catch((error) => console.error(error))
