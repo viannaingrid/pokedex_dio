0a57393d99d99dfc4bf2ddc6446bb790c68c8a91
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-12-02 16:10:03 -0300

    :bricks: update json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,7 @@
-
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -6,13 +6,11 @@ pokeApi.getPokemonDetail = (pokemon) => {
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => {
-            debugger
-            console.log(pokemonsDetails)
-        })
+        .then((pokemonsDetails) => pokemonsDetails)
 }
